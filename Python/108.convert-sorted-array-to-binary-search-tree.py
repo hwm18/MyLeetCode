d87fc5b1be7ef6ec7ccccc16207db512a6675250
Wenming Huang <hwm18h@outlook.com>
2019-05-16 09:34:42 -0700

    Solved 110.balanced-binary-tree.

--- Python/108.convert-sorted-array-to-binary-search-tree.py
+++ Python/108.convert-sorted-array-to-binary-search-tree.py
@@ -1,32 +1,83 @@
 #
-# @lc app=leetcode id=108 lang=python3
+# @lc app=leetcode id=108 lang=python
 #
 # [108] Convert Sorted Array to Binary Search Tree
 #
+# https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/
+#
+# algorithms
+# Easy (50.33%)
+# Likes:    1077
+# Dislikes: 106
+# Total Accepted:    256.1K
+# Total Submissions: 508.4K
+# Testcase Example:  '[-10,-3,0,5,9]'
+#
+# Given an array where elements are sorted in ascending order, convert it to a
+# height balanced BST.
+# 
+# For this problem, a height-balanced binary tree is defined as a binary tree
+# in which the depth of the two subtrees of every node never differ by more
+# than 1.
+# 
+# Example:
+# 
+# 
+# Given the sorted array: [-10,-3,0,5,9],
+# 
+# One possible answer is: [0,-3,9,-10,null,5], which represents the following
+# height balanced BST:
+# 
+# ⁠     0
+# ⁠    / \
+# ⁠  -3   9
+# ⁠  /   /
+# ⁠-10  5
+# 
+# 
+#
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution:
-    # Method 1: Your runtime beats 88.22 % of python3 submissions
-    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
-        if not nums:
+class Solution(object):
+    '''
+    # solution 2: Your runtime beats 51.44 % of python submissions
+    def sortedArrayToBST(self, num):
+        if not num:
             return None
-        
-        return self.helper(nums,0, len(nums)-1)
+
+        mid = len(num) // 2
+
+        root = TreeNode(num[mid])
+        root.left = self.sortedArrayToBST(num[:mid])
+        root.right = self.sortedArrayToBST(num[mid+1:])
+
+        return root
+    '''
+
+    # Your runtime beats 99.41 % of python submissions
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if not nums or len(nums)==0:
+            return None
+        return self.helper(nums,0,len(nums)-1)
 
     def helper(self, nums, start, end):
         if start > end:
             return None
         if start == end:
             return TreeNode(nums[start])
-        
-        mid = start + (end -start)//2
+
+        mid = start + (end - start)//2
         root = TreeNode(nums[mid])
-        root.left = self.helper(nums,start,mid-1)
+        root.left = self.helper(nums, start,mid-1)
         root.right = self.helper(nums, mid+1,end)
         return root
         
---
+++ Python/110.balanced-binary-tree.py
@@ -0,0 +1,82 @@
+#
+# @lc app=leetcode id=110 lang=python
+#
+# [110] Balanced Binary Tree
+#
+# https://leetcode.com/problems/balanced-binary-tree/description/
+#
+# algorithms
+# Easy (40.89%)
+# Likes:    1200
+# Dislikes: 105
+# Total Accepted:    316.9K
+# Total Submissions: 775K
+# Testcase Example:  '[3,9,20,null,null,15,7]'
+#
+# Given a binary tree, determine if it is height-balanced.
+# 
+# For this problem, a height-balanced binary tree is defined as:
+# 
+# 
+# a binary tree in which the depth of the two subtrees of every node never
+# differ by more than 1.
+# 
+# 
+# Example 1:
+# 
+# Given the following tree [3,9,20,null,null,15,7]:
+# 
+# 
+# ⁠   3
+# ⁠  / \
+# ⁠ 9  20
+# ⁠   /  \
+# ⁠  15   7
+# 
+# Return true.
+# 
+# Example 2:
+# 
+# Given the following tree [1,2,2,3,3,null,null,4,4]:
+# 
+# 
+# ⁠      1
+# ⁠     / \
+# ⁠    2   2
+# ⁠   / \
+# ⁠  3   3
+# ⁠ / \
+# ⁠4   4
+# 
+# 
+# Return false.
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    # Your runtime beats 79.85 % of python submissions
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        
+        return self.helper(root) !=-1
+    
+    def helper(self, root):
+        if not root:
+            return 0
+        l = self.helper(root.left)
+        r = self.helper(root.right)
+        if l==-1 or r ==-1 or abs(l -r)>1:
+            return -1
+        return max(l,r)+1
+
